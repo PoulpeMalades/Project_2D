d09c21d639566b746bda28023e9a0adfd7db1dee
Baptiste Gelly <b.gelly@ensi.lan>
2025-03-31 16:58:47 +0200

    Added Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,15 @@ public class PlayerController : MonoBehaviour
 {
     public float MoveSpeed;
     public float JumpForce;
+    public float AttackCooldown;
     
     private Rigidbody2D _rigidBody;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
+
+    private float _attackTimer;
+    private bool _isAttacking;
+
     
     void Start()
     {
@@ -21,20 +26,48 @@ public class PlayerController : MonoBehaviour
         bool isJumping = Input.GetButtonDown("Jump");
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        bool attack = Input.GetButton("Fire1");
         
-        // Horizontal movement
-        transform.Translate(horizontal * Time.deltaTime * MoveSpeed, 0, 0);
+        // Horizontal movement$
+        if (_attackTimer >= AttackCooldown)
+        {
+            transform.Translate(horizontal * Time.deltaTime * MoveSpeed, 0, 0);
+        }
         
         // Jump force
-        if (isJumping)
+        if (isJumping && _rigidBody.linearVelocity.y == 0 )
         {
             _rigidBody.AddForce(Vector2.up * JumpForce);
-            _animator.SetFloat("Vertical", Mathf.Abs(vertical));
+            _animator.SetBool("Jump", true);
         }
+        if (_rigidBody.linearVelocity.y < 0)
+        {
+             _animator.SetBool("Jump", false);
+        }
+
+        if (attack && _attackTimer >= AttackCooldown)
+        {
+            _attackTimer = 0;
+            _animator.SetBool("attack", true);
+            transform.Translate(0, 0, 0);
+        }
+        else
+        {
+            _attackTimer += Time.deltaTime;
+            _animator.SetBool("attack",false);
+        }
+        
+        //     
+         // if (isJumping && _rigidBody.linearVelocity.y < 0)
+        // {
+        //     _animator.SetBool("Jump", false);
+        // }
+             
+           
         
         //Animation update
         _animator.SetFloat("Horizontal", Mathf.Abs(horizontal));
-        
+       
         
         //Flipping sprite
         if (horizontal > 0)
@@ -43,4 +76,5 @@ public class PlayerController : MonoBehaviour
             _spriteRenderer.flipX = true;
         
     }
+    
 }
